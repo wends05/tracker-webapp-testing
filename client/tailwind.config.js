e3411775d83cd3e85dd558540698e3d2c8ded853
wends05 <cxnner05@gmail.com>
2024-11-02 17:05:54 +0800

    tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkCopper: "#292421",
+        copper: "#A75F37",
+        pink: "#CA8E82",
+        tan: "#D9B99F",
+        blush: "#F2D6C3",
+        vanilla: "#F2E7DD",
+        green: "#7A958F",
+        mint: "#BAE0DA",
+      },
+    },
   },
   plugins: [],
-}
-
+};
