4efb5093ba813b3e728d6c6080b2abef5a18ea8d
wends05 <cxnner05@gmail.com>
2024-11-05 17:52:38 +0800

    added tailwind config colors, and refactored sidebar

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -3,6 +3,21 @@ import logo from "../assets/logosample1.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBarsProgress, faXmark } from "@fortawesome/free-solid-svg-icons";
 
+const navLinks = [
+  {
+    name: "History",
+  },
+  {
+    name: "Calendar",
+  },
+  {
+    name: "Profile",
+  },
+  {
+    name: "About Us",
+  },
+];
+
 const Sidebar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
@@ -12,35 +27,10 @@ const Sidebar = () => {
 
   return (
     <div>
-      <header className="flex justify-between items-center text-black px-16 bg-white drop-shadow-md h-16">
+      <header className="flex justify-between items-center text-black px-16 bg-vanilla drop-shadow-md h-16">
         <button onClick={toggleMenu} className="text-2xl">
           <FontAwesomeIcon icon={faBarsProgress} />
         </button>
-
-        {/* <a href="#">
-          <img
-            src={logo}
-            alt="logo"
-            className="w-24 md:w-32 lg:w-40 h-auto transition-all"
-          />
-        </a> */}
-
-        {/* <ul className="hidden md:flex items-center gap-6 font-semibold text-base">
-          <li className="p-2 hover:bg-gray-400 hover:text-white rounded-md transition-all cursor-pointer">
-            Home
-          </li>
-          <li className="p-2 hover:bg-gray-400 hover:text-white rounded-md transition-all cursor-pointer">
-            History
-          </li>
-          <li className="p-2 hover:bg-gray-400 hover:text-white rounded-md transition-all cursor-pointer">
-            Profile
-          </li>
-          <li className="p-2 hover:bg-gray-400 hover:text-white rounded-md transition-all cursor-pointer">
-            About Us
-          </li>
-        </ul> */}
-
-        
       </header>
 
       <div
@@ -48,40 +38,21 @@ const Sidebar = () => {
           isMenuOpen ? "translate-x-0" : "-translate-x-full"
         }`}
       >
-        <button
-          onClick={toggleMenu}
-          className=" text-2xl w-min self-end"
-        >
-          <FontAwesomeIcon
-          
-          icon={faXmark} />
+        <button onClick={toggleMenu} className=" text-2xl w-min self-end">
+          <FontAwesomeIcon icon={faXmark} color="black" />
         </button>
 
         <nav className="flex flex-col items-start p-6 text-black">
-          <a
-            href="#"
-            className="py-2 px-2 text-lg font-semibold hover:bg-gray-200 w-full rounded-md"
-          >
-            History
-          </a>
-          <a
-            href="#"
-            className="py-2 px-2 text-lg font-semibold hover:bg-gray-200 w-full rounded-md"
-          >
-            Calendar
-          </a>
-          <a
-            href="#"
-            className="py-2 px-2 text-lg font-semibold hover:bg-gray-200 w-full rounded-md"
-          >
-            Profile
-          </a>
-          <a
-            href="#"
-            className="py-2 px-2 text-lg font-semibold hover:bg-gray-200 w-full rounded-md"
-          >
-            About Us
-          </a>
+          {navLinks.map(({ name }) => {
+            return (
+              <a
+                href="#"
+                className="py-2 px-2 text-lg font-semibold hover:bg-gray-200 w-full rounded-md"
+              >
+                {name}
+              </a>
+            );
+          })}
         </nav>
       </div>
 
