66ff917ae4dadedd39a8e22fb85917e8384ee335
wends05 <cxnner05@gmail.com>
2024-11-07 22:33:10 +0800

    added ui for editCategory and backend for PUT category/:id

--- client/src/routes/home/categories/EditCategory.tsx
+++ client/src/routes/home/categories/EditCategory.tsx
@@ -1,8 +1,8 @@
-import React, { useRef, useState } from "react";
+import React, { useState, useRef } from "react";
 import { CATEGORY_COLORS } from "../../../utils/constants";
 
-const EditCategory = () => {
-  const [categoryName, setCategoryName] = useState("");
+const AddCategory = () => {
+  const [categoryName, setCategoryName] = useState<string>("");
   const [budget, setBudget] = useState<number | "">("");
   const [backgroundColor, setBackgroundColor] = useState<string>("");
   const [backgroundImage, setBackgroundImage] = useState<File | null>(null);
@@ -11,8 +11,10 @@ const EditCategory = () => {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-
-    // implement edit category logic here
+    console.log("Category Name:", categoryName);
+    console.log("Budget:", budget);
+    console.log("Background Color:", backgroundColor);
+    console.log("Background Image:", backgroundImage);
   };
 
   const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -22,6 +24,7 @@ const EditCategory = () => {
       setImagePreviewUrl(URL.createObjectURL(file));
     }
   };
+
   const removeImage = () => {
     setBackgroundImage(null);
     setImagePreviewUrl(null);
@@ -32,114 +35,118 @@ const EditCategory = () => {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="max-w-lg mx-auto">
-        <h1 className="text-2xl text-black font-bold mb-4 text-center">
+      <form
+        onSubmit={handleSubmit}
+        className="max-w-lg w-full flex flex-col gap-2"
+      >
+        <h1 className="text-2xl text-black font-bold text-center">
           Edit Category
         </h1>
 
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label
-              htmlFor="categoryName"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Category Name:
-            </label>
+        <div>
+          <label
+            htmlFor="categoryName"
+            className="text-sm font-medium text-gray-700"
+          >
+            Category Name
+          </label>
+          <input
+            type="text"
+            id="categoryName"
+            value={categoryName}
+            onChange={(e) => setCategoryName(e.target.value)}
+            required
+            className="w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-blue-500"
+            placeholder="Enter category name"
+          />
+        </div>
+
+        <div>
+          <label
+            htmlFor="budget"
+            className="text-sm font-medium text-gray-700"
+          >
+            Budget
+          </label>
+          <input
+            type="number"
+            id="budget"
+            value={budget}
+            onChange={(e) => setBudget(Number(e.target.value))}
+            required
+            className="block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-blue-500"
+            placeholder="Enter budget"
+          />
+        </div>
+
+        <div>
+          <label className="text-sm font-medium text-gray-700">
+            Select Background Color
+          </label>
+          <div className="flex space-x-2 mt-1">
+            {CATEGORY_COLORS.map((color) => (
+              <div
+                key={color}
+                onClick={() => setBackgroundColor(color)}
+                className={`w-10 h-10 cursor-pointer rounded-full border-2 ${
+                  backgroundColor === color
+                    ? "border-blue-500"
+                    : "border-transparent"
+                }`}
+                style={{ backgroundColor: color }}
+              ></div>
+            ))}
+          </div>
+        </div>
+
+        <div>
+          <label className="block text-sm font-medium text-gray-700">
+            Upload Background Image
+          </label>
+          <div className="flex items-center">
             <input
-              type="text"
-              id="categoryName"
-              required
-              className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-blue-500"
-              value={categoryName}
-              onChange={(e) => setCategoryName(e.target.value)}
-              placeholder="Enter new category name"
+              type="file"
+              id="backgroundImage"
+              onChange={handleImageUpload}
+              ref={fileInputRef}
+              className="hidden"
             />
-          </div>
-          <div>
-            <label
-              htmlFor="budget"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Budget
-            </label>
-            <input
-              type="text"
-              id="budget"
-              required
-              className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-blue-500"
-              value={budget}
-              onChange={(e) => setBudget(Number(e.target.value))}
-              placeholder="Enter new budget"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="text-sm font-medium text-gray-700">
-              Select Background Color:
-            </label>
-            <div className="flex spae-x-2 mt-1">
-              {CATEGORY_COLORS.map((color) => (
-                <div
-                  key={color}
-                  onClick={() => setBackgroundColor(color)}
-                  className={`w-10 h-10 cursor-pointer rounded-full border-2 ${
-                    backgroundColor === color
-                      ? "border-blue-500"
-                      : "border-transparent"
-                  }`}
-                  style={{ backgroundColor: color }}
-                ></div>
-              ))}
-            </div>
-          </div>
-
-          <div className="mb-4">
-            <label htmlFor="" className="text-sm font-medium text-gray-700">
-              Upload Background Image:
-            </label>
-            <div className="flex items-center flex-col">
-              <input
-                type="file"
-                id="backgroundImage"
-                ref={fileInputRef}
-                className="hidden"
-              />
-              <button
-                type="button"
-                onClick={() => fileInputRef.current?.click()}
-                className="mt-1 bg-teal-800 text-white font-bold py-2 px-4 rounded-md"
-              >
-                Choose File
-              </button>
-            </div>
-
-            {imagePreviewUrl && (
-              <div className="relative mb-4">
-                <img
-                  src={imagePreviewUrl}
-                  alt="Preview"
-                  className="w-full h-64 object-cover rounded-md"
-                />
-                <button
-                  type="button"
-                  onClick={removeImage}
-                  className="absolute top-1 right-1 bg-gray-200 text-black rounded-full w-6 h-6 flex items-center justify-center hover:bg-red-600 transition duration-200"
-                >
-                  &times;
-                </button>
-              </div>
-            )}
-
             <button
-              type="submit"
-              className="w-full bg-teal-800 text-white font-semibold py-2 rounded-md hover:bg-blue-700 transition duration-200"
+              type="button"
+              onClick={() => fileInputRef.current?.click()}
+              className="mt-1 bg-teal-800 text-white font-bold py-2 px-4 rounded-md"
             >
-              Add Category
+              Choose File
             </button>
           </div>
-        </form>
-      </div>
+        </div>
+
+        {imagePreviewUrl && (
+          <div className="relative mb-4">
+            <img
+              src={imagePreviewUrl}
+              alt="Preview"
+              className="w-full h-64 object-cover rounded-md"
+            />
+            <button
+              type="button"
+              onClick={removeImage}
+              className="absolute top-1 right-1 bg-gray-200 text-black rounded-full w-7 h-7 flex items-center justify-center hover:bg-red-600 transition duration-200"
+            >
+              X
+            </button>
+          </div>
+        )}
+
+        <button
+          type="submit"
+          className="w-full bg-teal-800 text-white font-semibold py-2 rounded-md hover:bg-blue-700 transition duration-200"
+        >
+          Add Category
+        </button>
+      </form>
     </div>
   );
 };
 
-export default EditCategory;
+export default AddCategory;
