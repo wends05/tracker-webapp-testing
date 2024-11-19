6c2a5c59ec019999c72e3e94003bcad1febc3a8c
wends05 <cxnner05@gmail.com>
2024-11-19 18:09:38 +0800

    adjusted types for Expenses on server and client

--- client/src/types/Expense.ts
+++ client/src/types/Expense.ts
@@ -1,9 +1,10 @@
 type Expense = { 
-  expense_id: string,
-  name: string,
+  expense_id?: string,
+  expense_name: string,
   quantity: number,
   price: number,
   total: number,
+  category_id: number
 }
 
 export default Expense
