3cb9b904b468815f9e8651c6810c64da810db210
wends05 <cxnner05@gmail.com>
2024-11-22 02:56:06 +0800

    eslint spaces

--- eslint.config.js
+++ eslint.config.js
@@ -23,6 +23,6 @@ export default tseslint.config(
       "@typescript-eslint/no-explicit-any": "warn",
       "@typescript-eslint/no-require-imports": "off",
       "@typescript-eslint/no-unused-vars": "warn",
-    }, 
-  },
+    },
+  }
 );
--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -2,11 +2,10 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
@@ -14,6 +13,6 @@ export default [
       "@typescript-eslint/no-require-imports": "off",
       "@typescript-eslint/no-explicit-any": "warn",
       "@typescript-eslint/no-unused-vars": "warn",
-    }
-  }
+    },
+  },
 ];
