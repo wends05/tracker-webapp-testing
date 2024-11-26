0810f425cba7f672c0d8a602f932f5869320da19
cassandraysabel <141990919+cassandraysabel@users.noreply.github.com>
2024-11-26 12:44:23 +0800

    added post for expenseRouter

--- server/index.ts
+++ server/index.ts
@@ -3,13 +3,15 @@ import categoryRouter from "./routes/categories";
 import { pool } from "./db";
 import userRouter from "./routes/users";
 import cors from "cors";
+import expenseRouter from "./routes/expense";
 
 const app = express();
 const port = 3000;
-
+;
 app.use(express.json());
 app.use(cors());
 app.use(cors());
+app.use("/expense", expenseRouter);
 app.use("/category", categoryRouter);
 app.use("/user", userRouter);
 
--- server/routes/expense.ts
+++ server/routes/expense.ts
@@ -4,12 +4,33 @@ import Expense from "../types/Expense";
 
 const expenseRouter = express.Router();
 
+expenseRouter.post("", async (req: Request, res: Response) => {
+try {
+  console.log("p")
+  const {expense_name, price, quantity, total, category_id}: Expense = req.body
+  const result = await pool.query(
+    'INSERT INTO "Expense"(expense_name, price, quantity, total, category_id) VALUES($1, $2, $3, $4, $5) RETURNING *',
+    [expense_name, price, quantity, total, category_id]
+  )
+
+res.status(200).json({
+  data: result.rows[0]
+})
+
+} catch (error: any) {
+  res.status(500).json({
+    message: "An error has occured",
+    error: error.message,
+  });
+}
+})
+
 expenseRouter.put(":expense_id", async (req: Request, res: Response) => {
   try {
     const { expense_id } = req.params;
     const { expense_name, price, quantity, total }: Expense = req.body;
     const data = await pool.query(
-      'UPDATE "Expense" SET name = $1, price = $2, quantity = $3, total = $4 WHERE expense_id = $5 RETURNING *',
+      'UPDATE "Expense" SET expense_name = $1, price = $2, quantity = $3, total = $4 WHERE expense_id = $5 RETURNING *',
       [expense_name, price, quantity, total, expense_id]
     );
 
