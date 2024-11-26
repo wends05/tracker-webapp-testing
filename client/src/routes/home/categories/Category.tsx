fa78820e41a1d8c8db9cdf550246c14118c0fad6
Manawkie <samenianjoshua@gmail.com>
2024-11-26 22:48:43 +0800

    Fetch
    Added fetch in DB but still have problems in showing the expenses

--- client/src/routes/home/categories/Category.tsx
+++ client/src/routes/home/categories/Category.tsx
@@ -1,21 +1,40 @@
 import ExpenseBox from "@/components/Expense";
-import { Link, Outlet } from "react-router-dom";
+import { BackendResponse } from "@/interfaces/response";
+import Expense from "@/types/Expense";
+import { Category } from "@/utils/types";
+import { useQuery } from "@tanstack/react-query";
+import { Outlet, useLoaderData } from "react-router-dom";
 
-const Category = () => {
+const CategoryPage = () => {
+  const { data: category } = useLoaderData() as BackendResponse<Category>;
+  const { data: expenses } = useQuery<Expense[]>({
+    queryKey: ["category", category.category_id, "expenses"],
+    queryFn: () =>
+      fetch(
+        `http://localhost:3000/category/${category.category_id}/expenses`
+      ).then(async (res) => {
+        console.log("working")
+        console.log(await res.json())
+        return await res.json()}),
+  });
   return (
     <div className="relative flex h-full items-center justify-center">
-      <h1 className="mb-4 text-center text-2xl font-bold text-black">
-        
-      </h1>
+      <h1 className="mb-4 text-center text-2xl font-bold text-black"></h1>
       {/* <Link to={"expense/add"}>Add Expense</Link> */}
 
-
       <div>
-        <ExpenseBox
-        
-        
-        
-        />
+        {expenses && expenses.map((expense) => (
+          <ExpenseBox
+            category_id={expense.category_id}
+            price={expense.price}
+            expense_name={expense.expense_name}
+            quantity={expense.quantity}
+            total={expense.total}
+            expense_id={Number(expense.expense_id)}
+            key={expense.expense_id}
+
+          />
+        ))}
       </div>
       <div>
         <Outlet />
@@ -24,4 +43,4 @@ const Category = () => {
   );
 };
 
-export default Category;
+export default CategoryPage;
