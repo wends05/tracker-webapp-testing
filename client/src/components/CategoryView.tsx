c6a870d58ffb0c11dfb81306048b463965650ee4
cassandraysabel <141990919+cassandraysabel@users.noreply.github.com>
2024-12-03 11:33:42 +0800

    changed icons

--- client/src/components/CategoryView.tsx
+++ client/src/components/CategoryView.tsx
@@ -1,6 +1,6 @@
 import { Category } from "@/utils/types";
 import { Link } from "react-router-dom";
-import { PencilSquareIcon } from "@heroicons/react/24/outline";
+import { PencilLine } from "lucide-react";
 
 const CategoryView = (category: { category: Category }) => {
   const { category_id, category_color, category_name, budget } =
@@ -21,10 +21,10 @@ const CategoryView = (category: { category: Category }) => {
         </div>
       </Link>
       <Link
-        className="absolute right-2 top-2 rounded bg-rose-500 px-2 py-1 text-xs text-white hover:bg-rose-600"
+        className="absolute right-2 top-2 rounded px-2 py-1 text-xs text-white"
         to={`category/${category_id}/edit`}
       >
-        <PencilSquareIcon className="h-6 w-6" />
+        <PencilLine className="h-6 w-6" />
       </Link>
     </div>
   );
