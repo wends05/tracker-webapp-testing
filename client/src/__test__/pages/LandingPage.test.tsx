d1f3f7dd2270a90a341822f6d532e96488e79c96
wends05 <cxnner05@gmail.com>
2024-12-16 00:47:35 +0800

    fixed auth functions from AuthPage and added test cases

--- client/src/__test__/pages/LandingPage.test.tsx
+++ client/src/__test__/pages/LandingPage.test.tsx
@@ -1,7 +1,13 @@
 import { beforeEach, describe, expect, it, vi } from "vitest";
-import { render, screen } from "@testing-library/react";
+import {
+  fireEvent,
+  render,
+  renderHook,
+  screen,
+  waitFor,
+} from "@testing-library/react";
 import Landing from "@/routes/Landing";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
 
 beforeEach(() => {
   // IntersectionObserver isn't available in test environment
@@ -21,5 +27,23 @@ describe("LandingPage", () => {
 
     expect(landingTitle).toBeDefined();
     expect(landingTitle.innerHTML).toContain("Wise tracking");
+  });
+
+  it("should allow navigation to the auth page", async () => {
+    render(<Landing />, { wrapper: BrowserRouter });
+
+    const { result } = renderHook(() => useLocation(), {
+      wrapper: BrowserRouter,
+    });
+
+    const linkButton = await screen.findByTestId("auth-link");
+
+    fireEvent.click(linkButton);
+
+    expect(linkButton).toBeDefined();
+
+    waitFor(() => {
+      expect(result.current.pathname).toBe("/auth");
+    });
   });
 });
--- client/src/routes/Landing.tsx
+++ client/src/routes/Landing.tsx
@@ -45,7 +45,7 @@ const Landing = () => {
                   tellus. Bibendum dignissim sagittis sapien porta id posuere.
                 </p>
 
-                <Link to={"/auth"}>
+                <Link to={"/auth"} data-testid="auth-link">
                   {" "}
                   <Button className="mt-5 p-5 opacity-75">Get Started </Button>
                 </Link>
