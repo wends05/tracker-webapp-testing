2c8eb2242c2221bfddcd5bef01885af4c7348370
wends05 <cxnner05@gmail.com>
2024-12-16 16:37:19 +0800

    initially added content to the about page

--- client/src/routes/About.tsx
+++ client/src/routes/About.tsx
@@ -1,5 +1,23 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center gap-10 px-10 md:px-20">
+      <div className="flex flex-col items-center justify-center gap-5">
+        <h1>About this project</h1>
+        <p>
+          This project aims to provide a well organized and easy to use
+          budgeting web application by segregating expenses into categories with
+          weekly split budgets.
+        </p>
+
+        <p>
+          The project aims to help users visualize expenses in groups of weeks
+          and categories, allowing then to analyze their spending habits and
+          make informed decisions on how to manage their finances.
+        </p>
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default About;
