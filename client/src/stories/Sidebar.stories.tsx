ec33a088a19ef7df9c36826d839afea0aa0d7c7c
wends05 <cxnner05@gmail.com>
2025-02-16 22:58:28 +0800

    started on sidebar stories

---
+++ client/src/stories/Sidebar.stories.tsx
@@ -0,0 +1,14 @@
+import Sidebar from "@/components/Sidebar";
+import { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  component: Sidebar,
+} satisfies Meta<typeof Sidebar>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  
+}
