78c31329249e0c5aa85bf3af7f2f650c295331f3
matchaejayyy <145851990+matchaejayyy@users.noreply.github.com>
2025-02-17 11:05:04 +0800

    updated the Summarycard.tsx

--- client/src/stories/SummaryCard.stories.tsx
+++ client/src/stories/SummaryCard.stories.tsx
@@ -1,32 +1,20 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import SummaryCard from "../components/SummaryCard";
-import { Category } from "@/utils/types";
-import { fn } from "@storybook/test";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-// Create a new QueryClient instance
 const queryClient = new QueryClient();
 
-const sampleCategories: Category[] = [
-  {
-    amount_left: 100,
-    amount_spent: 0,
-    budget: 100,
-    category_color: "#FF0000",
-    category_name: "Food",
-    description: "Food and Groceries",
-    user_id: 1,
-    category_id: 1,
-  },
-];
 
 const meta: Meta<typeof SummaryCard> = {
   component: SummaryCard,
   args: {
     summary: {
-      categories: sampleCategories,
-      onSort: fn(),
       weekly_summary_id: 123,
+      date_start: new Date(),
+      date_end: new Date(),
+      total_budget: 0,
+      total_spent: 0,
+      total_not_spent: 0
     },
   },
   tags: ["autodocs"],
@@ -46,9 +34,12 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     summary: {
-      categories: [],
-      onSort: fn(),
       weekly_summary_id: 123,
+      date_start: new Date(),
+      date_end: new Date(),
+      total_budget: 500,
+      total_spent: 400,
+      total_not_spent: 100
     },
   },
 };
